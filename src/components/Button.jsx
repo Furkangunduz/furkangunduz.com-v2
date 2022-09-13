f50d2f9ed0d3df1057d95b0adf3fbd880354e68a
Furkan <furkan.gundz00@gmail.com>
2022-09-13 18:38:51 +0300

    Make button downloandable.

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 
-function Button({ children }) {
+function Button({ children, href = '', downloadable = false }) {
 	return (
-		<button className='text-buttonText whitespace-nowrap bg-buttonBg px-3 py-3 rounded-md'>
-			{children}
-		</button>
+		<a href={href} download={downloadable}>
+			<button className='text-buttonText whitespace-nowrap bg-buttonBg px-3 py-3 rounded-md hover:scale-[1.1] hover:-translate-y-3 transition-all duration-[300ms]'>
+				{children}
+			</button>
+		</a>
 	);
 }
 
